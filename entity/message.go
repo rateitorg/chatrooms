fcf159e17349ad4956f62b0b03ed0832d0573343
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-28 23:37:58 +0000

    feat: create message entity
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ entity/message.go
@@ -0,0 +1,11 @@
+package entity
+
+// Message represents a message sent by a client.
+type Message struct {	
+	// Message content
+	Content string `json:"content"`
+	// Message sender name
+	SenderName string `json:"sender"`
+	// Message sent time
+	SentTime string `json:"sentTime"`
+}
