a6d24193b230bf3eaca50f6628e665cac54bd76f
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-29 02:19:14 +0000

    feat: register and unregister clients
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- server/main.go
+++ server/main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rateitorg/chatrooms/handler"
+	"github.com/rateitorg/chatrooms/service"
 )
 
+// Entry point of the application
 func main() {
+	// Set up gin engine
 	engine := setUpEngine()
+
+	// Create a new hub
+	hub := service.NewHub()
+
+	// Set up routes
+	// Default route
+	engine.GET("ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// WebSocket route
+	engine.GET("/ws", func(ctx *gin.Context) {
+		handler.WebSocketHandler(hub, ctx.Writer, ctx.Request)
+	})
 
 	// Start engine
 	if err := engine.Run(":8080"); err != nil {
 		panic(err)
 	}
 }
-
 
 // Create a new gin engine
 func setUpEngine() *gin.Engine {
--- service/hub.go
+++ service/hub.go
@@ -7,15 +7,37 @@ import (
 // Hub will hold a set of active clients and broadcast messages.
 type Hub struct {
 	// Registered clients.
-	// TODO: add a map to store clients
+	Clients map[*Client]bool
 
 	// Inbound messages from the clients.
 	Broadcast chan []entity.Message
+
+	// Register requests from the clients.
+	Register chan *Client
+
+	// Unregister requests from clients.
+	Unregister chan *Client
 }
 
 // NewHub creates a new hub.
 func NewHub() *Hub {
 	return &Hub{
 		Broadcast: make(chan []entity.Message),
+	}
+}
+
+// Hub Event Loop
+func (h *Hub) Run() {
+	// Continuously listen for events
+	for {
+		select {
+		case client := <-h.Register: // If there is something in the register channel
+			h.Clients[client] = true
+		case client := <-h.Unregister: // If there is something in the unregister channel
+			if _, ok := h.Clients[client]; ok {
+				delete(h.Clients, client) // Remove from map
+				close(client.Send) // Close the send channel
+			}
+		}
 	}
 }
