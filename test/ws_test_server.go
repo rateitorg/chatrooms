6a8699e5045bb17ceafe03ee8c857713276178a6
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-12-01 18:07:28 +0000

    feat(test): add test for client write
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- test/ws_test_server.go
+++ test/ws_test_server.go
@@ -5,17 +5,17 @@ import (
 	"net/http/httptest"
 
 	"github.com/gorilla/websocket"
+	"github.com/rateitorg/chatrooms/entity"
+	"github.com/rateitorg/chatrooms/service"
 )
 
 type TestServer struct {
 	Server   *httptest.Server
-	Upgrader websocket.Upgrader
 	URL      string
 }
 
-// NewTestServer creates a new test server.
-func NewTestServer(handlerFunc http.HandlerFunc) *TestServer {
-	upgrader := websocket.Upgrader{}
+// NewTestServer creates a new test server with custom handler.
+func NewTestServerWithCustomHandler(handlerFunc http.HandlerFunc) *TestServer {
 
 	// Create a test server with the handler function
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +24,35 @@ func NewTestServer(handlerFunc http.HandlerFunc) *TestServer {
 
 	return &TestServer{
 		Server:   server,
-		Upgrader: upgrader,
+		URL:      "ws" + server.URL[len("http"):],
+	}
+}
+
+// NewTestServer creates a new test server with client write running.
+func NewTestServerWithClientWrite(hub service.HubInterface) *TestServer {
+	// Create a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		upgrader := websocket.Upgrader{}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+
+		client := &service.Client{
+			Hub:  hub,
+			Conn: conn,
+			Send: make(chan entity.Message),
+		}
+
+		// Register the client
+		client.Hub.SendToRegisterChannel(client)
+
+		// Start the client's write goroutines
+		go client.Write()
+	}))
+
+	return &TestServer{
+		Server:   server,
 		URL:      "ws" + server.URL[len("http"):],
 	}
 }
